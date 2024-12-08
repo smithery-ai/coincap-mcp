4c074f55eb5c9c3c60c0d74ce1d0826fa6f9d6ca
Alex Andru <alex007d@gmail.com>
2024-12-09 00:46:54 +0100

    chore: bump version

--- src/constants.ts
+++ src/constants.ts
@@ -2,5 +2,5 @@ export const BITCOIN_PRICE_URL = "https://api.coincap.io/v2/assets/bitcoin";
 export const CONSTANTS = {
   CRYPTO_PRICE_URL: "https://api.coincap.io/v2/assets/",
   PROJECT_NAME: "coincap-mcp",
-  PROJECT_VERSION: "0.3.0",
+  PROJECT_VERSION: "0.4.0",
 };

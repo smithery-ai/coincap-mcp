2ea5faa09b354ee67c9350db3183f76e25ecc433
Alex Andru <alex007d@gmail.com>
2024-12-08 21:37:24 +0100

    feat: add constants

---
+++ src/constants.ts
@@ -0,0 +1,6 @@
+export const BITCOIN_PRICE_URL = "https://api.coincap.io/v2/assets/bitcoin";
+export const CONSTANTS = {
+  CRYPTO_PRICE_URL: "https://api.coincap.io/v2/assets/",
+  PROJECT_NAME: "coincap-mcp",
+  PROJECT_VERSION: "0.1.0",
+};
